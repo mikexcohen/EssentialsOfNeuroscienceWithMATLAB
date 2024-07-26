b690b5903e331fa3fa75d0a81e1165c7540633ff
Mike X Cohen <72656291+mikexcohen@users.noreply.github.com>
2024-07-26 10:49:24 +0300

    Update matlab4neuros_module2_partial.m

--- module2_EEG/matlab4neuros_module2_partial.m
+++ module2_EEG/matlab4neuros_module2_partial.m
@@ -30,7 +30,7 @@ close all; clear; clc
 %% Load the data
 
 % download from online and unpack
-websave('file.zip','https://sincxpress.com/neuroscience/matlab4neuros_module2.zip');
+websave('file.zip','https://sincxpress.com/neuroscience/matlab4neuros_module2.zip',weboptions('Timeout',120));
 unzip('file.zip');
 
 
